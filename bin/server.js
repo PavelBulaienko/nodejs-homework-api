ba1052210cdd36a6f15beee92a75545830ef0956
PavelBulaienko <57744770+PavelBulaienko@users.noreply.github.com>
2021-09-12 21:51:53 +0300

    add mongoDb

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,21 @@
-const app = require('../app')
+const app = require("../app");
 
-const PORT = process.env.PORT || 3000
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`);
+// });
+
+const mongoose = require("mongoose");
+const { DB_URL } = require("../config");
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
